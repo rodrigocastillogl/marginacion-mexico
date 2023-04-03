fix: correct cumulative explained variance;      correct call to preprocessing.scale();      correct data_path.

pca.py
@@ -13,7 +13,7 @@ df2020 = pd.read_csv(data_path)
 X = np.array( df2020.loc[:,'ANALF':'PO2SM'] )
 
 # data standardization
-Xc = preprocessing(X, axis = 0)
+Xc = preprocessing.scale(X, axis = 0)
 
 # PCA
 pca = PCA()
@@ -29,7 +29,8 @@ variance = pca.explained_variance_
 variance_ratio = pca.explained_variance_ratio_
 
 # ------------------- PCA results table ------------------ #
-print('Tabla de varianza expliacada')
+print()
+print('PCA  :  Tabla de varianza expliacada')
 print('-' * 70)
 print( ' {}  |   {}   |   {}   |   {}'.format( 'Componente', 'Valor propio'   ,
                                                '% de varianza', '% acumulado' ) )
@@ -37,10 +38,11 @@ print('-' * 70)
 
 c = 0
 for i in range( variance.shape[0] ):
-    c += variance[i] * 100
+    c += variance_ratio[i] * 100
     print( '{:^10d}   |       {:^.5}      |        {:^.5}      |       {:^.5}'.format(
            i + 1, '{:0.5f}'.format(variance[i]) , '{:0.5f}'.format( variance_ratio[i]*100 ) , '{:0.5f}'.format(c) ) )
 print( '-' * 70 )
+print()
 
 # To LaTex
 # pc = pd.DataFrame( { 'Componente':range(1,10), 'Valor propio':values, '% de varianza': variance*100, '% acumulado':acc}  )